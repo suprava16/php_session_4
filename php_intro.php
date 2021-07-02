3bbc35849d1c64ec206e26b79b7b01cb159869d3
suprava padhi <suprava.appstone@gmail.com>
2021-07-02 16:53:30 +0530

    php intro

---
+++ php_intro.php
@@ -0,0 +1,23 @@
+<!-- <?php
+echo "welcome to nextstacks";
+?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+
+</head>
+<body>
+<h1>heading</h1>
+  <?php
+    echo "hello from html tag";
+   
+    echo "<br>";
+  ?>
+
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,17 @@
+<?php
+
+$a=10;
+$a=20.5;
+$b="suprava";
+$c='hello';
+
+echo $a;
+echo "<br>";
+echo $b;
+
+echo "<br> value of a=",$a."<br>";
+echo "value of a=$a and values of b=$b"."<br>";
+echo "new line <br>";
+echo "value of a=$a and values of b=$b"."<br>";
+echo "welcome to nextstacks 'sriya'";
+?>
